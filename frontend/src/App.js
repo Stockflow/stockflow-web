bd6fdef7a0119fdf74ba7d400ef86549a10d0d0f
SplittyDev <splittydev@protonmail.com>
2020-02-19 15:22:17 +0100

    Modify frontend skeleton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
 
-function App() {
+function App ({ className }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className={className} />
+  )
 }
 
-export default App;
+export default styled(App)`
+`
